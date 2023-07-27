dc0348bec1f5035c1c1d59f5a81d8f5f1c14697a
Meghan Johnson <mjohnson2898@gmail.com>
2023-07-27 12:09:54 -0500

    Added some comments

--- src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/CompensationServiceImpl.java
@@ -15,6 +15,14 @@ public class CompensationServiceImpl implements CompensationService {
 
     @Autowired
     CompensationRepository compensationRepository;
+
+    /***
+     * Function to insert a new compensation into the Compensation Table
+     *
+     * @param compensation the body of the create request
+     * @return the original compensation
+     * @throws RuntimeException if the employee passed in does not exist in the Employee Table
+     */
     @Override
     public Compensation create(Compensation compensation) {
         if(employeeRepository.findByEmployeeId(compensation.getEmployee().getEmployeeId()) == null){
@@ -24,6 +32,13 @@ public class CompensationServiceImpl implements CompensationService {
         return compensation;
     }
 
+    /***
+     * Queries the database by the employeeId of the employee field of the compensation
+     *
+     * @param id the employeeId passed in through the request
+     * @return the compensation that contains the id in the employee field
+     * @throws RuntimeException if the query doesn't return a Compensation
+     */
     @Override
     public Compensation read(String id) {
         Compensation compensation = compensationRepository.findByEmployee_EmployeeId(id);
--- src/main/java/com/mindex/challenge/service/impl/ReportServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/ReportServiceImpl.java
@@ -14,12 +14,20 @@ public class ReportServiceImpl implements ReportService {
 
     @Autowired
     EmployeeRepository employeeRepository;
+
+    /**
+     * A function to create a reporting structure for an employeeId passed in through the endpoint.
+     * @param id the employeeId that the ReportingStructure is being created for
+     * @return A ReportingStructure
+     * @throws RuntimeException if the Employee does not exist in the Employee Repository
+     */
     @Override
     public ReportingStructure getReport(String id) {
         Employee employee = employeeRepository.findByEmployeeId(id);
         if (employee == null) {
             throw new RuntimeException("Invalid employeeId: " + id);
         }
+        //initial call to the recursive function
         int numberOfReports = getDirectReports(id);
 
         ReportingStructure reportingStructure = new ReportingStructure();
@@ -29,6 +37,12 @@ public class ReportServiceImpl implements ReportService {
         return reportingStructure;
     }
 
+    /**
+     * A recursive function to go down the tree of direct reports of a direct report.
+     * @param id the employeeId to look up
+     * @return the sum of reports that single employee has
+     * @throws RuntimeException if an employee does not exist in the Employee Repository
+     */
     private int getDirectReports(String id) {
 
         Employee employee = employeeRepository.findByEmployeeId(id);
@@ -36,11 +50,14 @@ public class ReportServiceImpl implements ReportService {
             throw new RuntimeException("Invalid employeeId: " + id);
         }
 
+        // If direct reports is null or 0, there is no need to continue
         if(employee.getDirectReports() == null || employee.getDirectReports().size() == 0) {
             return 0;
         }
         else{
+            // the initial size is the size of the list
             int sum = employee.getDirectReports().size();
+            // recalls this function for each direct report and sees if they have any direct reports
             for (Employee report : employee.getDirectReports()) {
                 sum += getDirectReports(report.getEmployeeId());
             }
